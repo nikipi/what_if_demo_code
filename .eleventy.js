1b645b9d60dc8c16d840869c6069e356520055b8
Apple <apple@AppledeMacBook-Air-74.local>
2020-07-27 14:40:45 +0200

    add

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+const markdownIt = require("markdown-it");
+const markdownItRenderer = new markdownIt();
+const rootPrefix = "";
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.templateFormats = ["liquid", "md", "png"];
+  
+  eleventyConfig.addFilter('markdownify', (str) => {
+    return markdownItRenderer.renderInline(str);
+  })
+
+  eleventyConfig.addShortcode("root", () => {
+    return rootPrefix;
+  });
+
+  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/demos/");
+};
